ecb3a8ef007bf4907ac6148de64a18125d968873
Kunal Chaturvedi <kunalmanu2468@gmail.com>
2024-04-18 02:45:47 +0530

    clerk auth and user management setup

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformations = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UpdateTransformations
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationsType = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddTransformationsType
