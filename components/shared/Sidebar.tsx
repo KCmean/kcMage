2f1cdbdd955ae4902dbffc2f887d220e057add43
Kunal Chaturvedi <kunalmanu2468@gmail.com>
2024-04-18 20:41:53 +0530

    sidebar feature

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -3,9 +3,10 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { navLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 
 
 const Sidebar = () => {
@@ -22,26 +23,58 @@ const Sidebar = () => {
                     <SignedIn>
 
                         <ul className='sidebar-nav_elements'>
-                            {navLinks.map((link) => {
+                            {navLinks.slice(0, 6).map((link) => {
 
                                 const isActive = link.route === pathname
                                 return (
                                     <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gr'}`}>
-                                            <Link className='sidebar-link' href={link.route}>
-                                                <Image
-                                                    src = {link.icon}
-                                                    alt = 'logo'
-                                                    width = {24}
-                                                    height = {24}
-                                                    className = {`${isActive && 'brightness-200'}`}
-                                                    />
-                                                {link.label}
-                                            </Link>
+                                        <Link className='sidebar-link' href={link.route}>
+                                            <Image
+                                                src={link.icon}
+                                                alt='logo'
+                                                width={24}
+                                                height={24}
+                                                className={`${isActive && 'brightness-200'}`}
+                                            />
+                                            {link.label}
+                                        </Link>
                                     </li>
                                 )
                             })}
                         </ul>
+
+
+                        <ul className='sidebar-nav_elements '>
+                            {navLinks.slice(6).map((link) => {
+
+                                const isActive = link.route === pathname
+                                return (
+                                    <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gr'}`}>
+                                        <Link className='sidebar-link' href={link.route}>
+                                            <Image
+                                                src={link.icon}
+                                                alt='logo'
+                                                width={24}
+                                                height={24}
+                                                className={`${isActive && 'brightness-200'}`}
+                                            />
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                            <li className='flex-center cursor-pointer p-4 gap-2'>
+                                <UserButton afterSignOutUrl='/' showName />
+                            </li>
+                        </ul>
                     </SignedIn>
+
+
+                    <SignedOut>
+                        <Button asChild className='button bg-purple-gradient bg-cover'>
+                            <Link href='/sign-in'>Login</Link>
+                        </Button>
+                    </SignedOut>
 
                 </nav>
             </div>
